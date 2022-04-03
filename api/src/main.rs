47d10ec9f0bbbbd26df13059da57c4e768e22e14
Piotr Wieczorek <czorek@protonmail.com>
2022-04-04 01:35:33 +0200

    connect db

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,4 +1,24 @@
 #[macro_use] extern crate rocket;
+use rocket::http::Status;
+extern crate redis;
+use redis::{Commands, Connection};
+const COUNTER_INITIALIZATION_VALUE: i32 = 0;
+const COUNTER_MAX_VALUE: i32 = 1000;
+const COUNTER_MIN_VALUE: i32 = 0;
+
+
+fn get_connection()->redis::RedisResult<Connection>{
+    redis::Client::open("redis://database").unwrap().get_connection()
+}
+
+fn return_counter()->redis::RedisResult<i32>{
+    get_connection()?.get("counter_value")
+}
+
+fn initalize_counter() -> redis::RedisResult<()> {
+    let _ : () = get_connection()?.set_nx("counter_value", COUNTER_INITIALIZATION_VALUE)?;
+    Ok(())
+}
 
 #[get("/")]
 fn index() -> &'static str {
@@ -6,27 +26,43 @@ fn index() -> &'static str {
 }
 
 #[get("/get_counter")]
-fn get_counter(){
-    todo!();
+fn get_counter()-> String {
+   format!("value: {}", return_counter().unwrap())
 }
 
 #[post("/set_counter/<value>")]
-fn set_counter(value: i32){
-    todo!();
+fn set_counter(value: i32) -> Status{
+    if !(COUNTER_MIN_VALUE..=COUNTER_MAX_VALUE).contains(&value){
+        return Status::NotAcceptable;
+    }
+    let _:() = get_connection().unwrap().set_nx("counter_value", value).unwrap();
+    Status::Ok
 }
 
 #[post("/increment")]
-fn increment(){
-    todo!();
+fn increment() -> Status{
+    let counter = return_counter();
+    if counter.is_err(){
+        return Status::new(500);
+    }
+    if counter == Ok(COUNTER_MAX_VALUE) {
+        return Status::NotAcceptable;
+    }
+    let _: () = get_connection().unwrap().incr("counter_value",1).unwrap();
+    Status::Ok
 }
 
 #[post("/decrement")]
-fn decrement(){
-    todo!();
+fn decrement()-> Status{
+    if return_counter() == Ok(COUNTER_MIN_VALUE) {
+        return Status::NotAcceptable;
+    }
+    let _: () = get_connection().unwrap().decr("counter_value",1).unwrap();
+    Status::Ok
 }
-
 
 #[launch]
 fn rocket() -> _ {
+    initalize_counter().unwrap();
     rocket::build().mount("/", routes![index, get_counter, set_counter, increment, decrement])
 }
