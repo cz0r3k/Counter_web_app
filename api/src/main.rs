0693ddbd66b7d145e83fca6d71ea46f0cd0a5b5d
Piotr Wieczorek <czorek@protonmail.com>
2022-04-03 22:31:47 +0200

    add endpoints

--- api/src/main.rs
+++ api/src/main.rs
@@ -5,6 +5,27 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/get_counter")]
+fn get_counter(){
+    todo!();
+}
+
+#[post("/set_counter/<value>")]
+fn set_counter(value: i32){
+    todo!();
+}
+
+#[post("/increment")]
+fn increment(){
+    todo!();
+}
+
+#[post("/decrement")]
+fn decrement(){
+    todo!();
+}
+
+
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/", routes![index])
