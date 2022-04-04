57c16a88b8ef83a9cb6878f043b663f3d2d2c3e1
Piotr Wieczorek <czorek@protonmail.com>
2022-04-05 01:52:53 +0200

    add swagger

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,15 +1,17 @@
 #[macro_use] extern crate rocket;
-#[macro_use] extern crate serde_json;
-use rocket::http::Status;
 extern crate redis;
 use redis::{Commands, Connection};
-const COUNTER_INITIALIZATION_VALUE: i32 = 0;
-const COUNTER_MAX_VALUE: i32 = 1000;
-const COUNTER_MIN_VALUE: i32 = 0;
-
+use rocket::http::Status;
 use rocket::http::Header;
 use rocket::{Request, Response};
 use rocket::fairing::{Fairing, Info, Kind};
+use rocket::serde::json::{json, Value};
+use rocket_okapi::settings::UrlObject;
+use rocket_okapi::{openapi, openapi_get_routes, rapidoc::*, swagger_ui::*};
+
+const COUNTER_MAX_VALUE: i32 = 1000;
+const COUNTER_MIN_VALUE: i32 = 0;
+const COUNTER_INITIALIZATION_VALUE: i32 = 0;
 
 pub struct CORS;
 
@@ -38,33 +40,57 @@ fn return_counter()->redis::RedisResult<i32>{
     get_connection()?.get("counter_value")
 }
 
-fn initalize_counter() -> redis::RedisResult<()> {
+fn set(value:i32) -> redis::RedisResult<()>{
+    let _:() = get_connection().unwrap().set("counter_value", value)?;
+    Ok(())
+}
+
+fn increment() -> redis::RedisResult<()>{
+    let _: () = get_connection().unwrap().incr("counter_value",1)?;
+    Ok(())
+}
+
+fn decrement() -> redis::RedisResult<()>{
+    let _: () = get_connection().unwrap().decr("counter_value",1)?;
+    Ok(())
+}
+
+fn initalize() -> redis::RedisResult<()> {
     let _ : () = get_connection()?.set_nx("counter_value", COUNTER_INITIALIZATION_VALUE)?;
     Ok(())
 }
 
+#[openapi(skip)]
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+/// # Get current counter value
+#[openapi(tag = "Counter")]
 #[get("/get_counter")]
-fn get_counter()-> String {
-    let obj = json!({"value":return_counter().unwrap()});
-    serde_json::to_string_pretty(&obj).unwrap()
+fn get_counter()-> Value {
+    json!({"value":return_counter().unwrap()})
 }
 
+/// # Set counter value
+#[openapi(tag = "Counter")]
 #[post("/set_counter/<value>")]
 fn set_counter(value: i32) -> Status{
     if !(COUNTER_MIN_VALUE..=COUNTER_MAX_VALUE).contains(&value){
         return Status::NotAcceptable;
     }
-    let _:() = get_connection().unwrap().set("counter_value", value).unwrap();
+    let operation =set(value);
+    if operation.is_err(){
+        return Status::new(500);
+    }
     Status::Ok
 }
 
+/// # Increment counter value by 1
+#[openapi(tag = "Counter")]
 #[post("/increment")]
-fn increment() -> Status{
+fn increment_counter() -> Status{
     let counter = return_counter();
     if counter.is_err(){
         return Status::new(500);
@@ -72,21 +98,61 @@ fn increment() -> Status{
     if counter == Ok(COUNTER_MAX_VALUE) {
         return Status::NotAcceptable;
     }
-    let _: () = get_connection().unwrap().incr("counter_value",1).unwrap();
+    let operation = increment();
+    if operation.is_err(){
+        return Status::new(500);
+    }
     Status::Ok
 }
 
+/// # Decrement counter value by 1
+#[openapi(tag = "Counter")]
 #[post("/decrement")]
-fn decrement()-> Status{
-    if return_counter() == Ok(COUNTER_MIN_VALUE) {
+fn decrement_counter()-> Status{
+    let counter = return_counter();
+    if counter.is_err(){
+        return Status::new(500);
+    }
+    if counter == Ok(COUNTER_MIN_VALUE) {
         return Status::NotAcceptable;
     }
-    let _: () = get_connection().unwrap().decr("counter_value",1).unwrap();
+    let operation = decrement();
+    if operation.is_err(){
+        return Status::new(500);
+    }
     Status::Ok
+}
+
+
+fn swagger_config() -> SwaggerUIConfig {
+    SwaggerUIConfig {
+        url: "../openapi.json".to_string(),
+        ..Default::default()
+    }
+}
+
+fn rapidoc_config() -> RapiDocConfig {
+    RapiDocConfig {
+        general: GeneralConfig {
+            spec_urls: vec![UrlObject::new("General", "../openapi.json")],
+            ..Default::default()
+        },
+        hide_show: HideShowConfig {
+            allow_spec_url_load: false,
+            allow_spec_file_load: false,
+            ..Default::default()
+        },
+        ..Default::default()
+    }
 }
 
 #[launch]
 fn rocket() -> _ {
-    initalize_counter().unwrap();
-    rocket::build().mount("/", routes![index, get_counter, set_counter, increment, decrement]).attach(CORS)
+    initalize().unwrap();
+    rocket::build()
+    .mount("/api/", routes![index])
+    .mount("/api/", openapi_get_routes![get_counter, set_counter, increment_counter, decrement_counter])
+    .mount("/api/swagger/", make_swagger_ui(&swagger_config()))
+    .mount("/api/rapidoc/", make_rapidoc(&rapidoc_config()))
+    .attach(CORS)
 }
