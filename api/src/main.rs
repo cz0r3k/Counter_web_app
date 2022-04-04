e92ebae56cf8a064b7c655f00f52242bb6834a44
Piotr Wieczorek <czorek@protonmail.com>
2022-04-04 15:30:04 +0200

    add cors to api

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,4 +1,5 @@
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate serde_json;
 use rocket::http::Status;
 extern crate redis;
 use redis::{Commands, Connection};
@@ -6,6 +7,28 @@ const COUNTER_INITIALIZATION_VALUE: i32 = 0;
 const COUNTER_MAX_VALUE: i32 = 1000;
 const COUNTER_MIN_VALUE: i32 = 0;
 
+use rocket::http::Header;
+use rocket::{Request, Response};
+use rocket::fairing::{Fairing, Info, Kind};
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
 
 fn get_connection()->redis::RedisResult<Connection>{
     redis::Client::open("redis://database").unwrap().get_connection()
@@ -27,7 +50,8 @@ fn index() -> &'static str {
 
 #[get("/get_counter")]
 fn get_counter()-> String {
-   format!("value: {}", return_counter().unwrap())
+    let obj = json!({"value":return_counter().unwrap()});
+    serde_json::to_string_pretty(&obj).unwrap()
 }
 
 #[post("/set_counter/<value>")]
@@ -35,7 +59,7 @@ fn set_counter(value: i32) -> Status{
     if !(COUNTER_MIN_VALUE..=COUNTER_MAX_VALUE).contains(&value){
         return Status::NotAcceptable;
     }
-    let _:() = get_connection().unwrap().set_nx("counter_value", value).unwrap();
+    let _:() = get_connection().unwrap().set("counter_value", value).unwrap();
     Status::Ok
 }
 
@@ -64,5 +88,5 @@ fn decrement()-> Status{
 #[launch]
 fn rocket() -> _ {
     initalize_counter().unwrap();
-    rocket::build().mount("/", routes![index, get_counter, set_counter, increment, decrement])
+    rocket::build().mount("/", routes![index, get_counter, set_counter, increment, decrement]).attach(CORS)
 }
